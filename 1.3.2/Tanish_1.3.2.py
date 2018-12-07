dd86e6bf40739319005d1001102172e34b94ca46
tekotan <b.tanish@gmail.com>
2018-12-07 21:10:40 +0000

    Small change

--- 1.3.2/Tanish_1.3.2.py
+++ 1.3.2/Tanish_1.3.2.py
@@ -5,7 +5,7 @@ def add_tip(total, tip_percent):
 def hyp(leg_a, leg_b):
     """ Return the hypotenuse of a triangle with legs leg_a and leg_b"""
     return (leg_a**2 + leg_b**2)**0.5
-def mean(list_numbers):
+def mean(*list_numbers):
     total = 0
     for num in list_numbers:
         total+=num
@@ -17,5 +17,5 @@ def perimeter(base,height):
 print (add_tip(20,0.15))
 print (add_tip(30,0.15))
 print (hyp(3,4))
-print (mean([3,4,7]))
+print (mean(3,4,7))
 print (perimeter(3,4))
