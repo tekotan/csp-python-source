f06f46cbfa565562ee5147667edb1d7fd07f79f2
tekotan <b.tanish@gmail.com>
2019-01-16 21:25:15 +0000

    Final Changes

--- Text_Adventure/Baranwal_Vadali_Text_Adventure.py
+++ Text_Adventure/Baranwal_Vadali_Text_Adventure.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import time
 import sys
 import random
+from riddles import *
 
 class Story():
     """
@@ -288,51 +289,19 @@ class Story():
         and having multiple riddles
         """
         print("Here is the riddle: \n")
-        riddle_1 = "My first is often at the front door.\n" \
-                    "My second is found in the cereal family.\n" \
-                    "My third is what most people want. \n" \
-                    "My whole is one of the United States. \n" \
-                
-        riddle_2 = "My life can be measured in hours, \n" \
-                    "I serve by being devoured. \n" \
-                    "Thin, I am quick \n" \
-                    "Fat, I am slow \n" \
-                    "Wind is my foe."
-        riddle_3 = "It is greater than God and more evil than the devil.\n" \
-                    "The poor have it, the rich need it and if you eat it" \
-                    "you will die.\n" \
-                    "What is it? \n"
-        riddle_4 = "What always runs but never walks, often murmurs, never \n" \
-                    "talks, has a bed but never sleeps, has a mouth but never" \
-                    "eats?"
-        riddle_5 = "What begins and has no end?" \
-                    "What is the ending of all that begins?"
-        riddle_list = [
-            riddle_1, 
-            riddle_2, 
-            riddle_3, 
-            riddle_4, 
-            riddle_5
-        ]
-        riddle_ans = [
-            "matrimony", 
-            "candle" , 
-            "nothing" , 
-            "river", 
-            "death"
-        ]
         riddle = random.choice(riddle_list)
         riddle_num = riddle_list.index(riddle)
         print(riddle)
-        response = raw_input("Answer:         ").lower()
+        response = raw_input("Answer:").lower()
         
         if response == riddle_ans[riddle_num]:
             print(
                 "Congratulation! You got the right answer! However, I ",
                 "am going to eat you! You kids will be stuck here ", 
-                "forever.You will never win. That is why I am a bad witch! ", 
+                "forever.You will never win. That is why I am a bad witch! \n", 
                 sep="\n"
             )
+            time.sleep(1)
             print(
                 "Crash! The door breaks! Comes in is a voracious dragon. ", 
                 "Fire coming everywhere. It came to save you, since you ", 
---
+++ Text_Adventure/riddles.py
@@ -0,0 +1,33 @@
+riddle_1 = "My first is often at the front door.\n" \
+            "My second is found in the cereal family.\n" \
+            "My third is what most people want. \n" \
+            "My whole is one of the United States. \n" \
+        
+riddle_2 = "My life can be measured in hours, \n" \
+            "I serve by being devoured. \n" \
+            "Thin, I am quick \n" \
+            "Fat, I am slow \n" \
+            "Wind is my foe."
+riddle_3 = "It is greater than God and more evil than the devil.\n" \
+            "The poor have it, the rich need it and if you eat it" \
+            "you will die.\n" \
+            "What is it? \n"
+riddle_4 = "What always runs but never walks, often murmurs, never \n" \
+            "talks, has a bed but never sleeps, has a mouth but never" \
+            "eats?"
+riddle_5 = "What begins and has no end?" \
+            "What is the ending of all that begins?"
+riddle_list = [
+    riddle_1, 
+    riddle_2, 
+    riddle_3, 
+    riddle_4, 
+    riddle_5
+]
+riddle_ans = [
+    "matrimony", 
+    "candle" , 
+    "nothing" , 
+    "river", 
+    "death"
+]
